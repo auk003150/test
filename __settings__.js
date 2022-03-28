046bf789a838e4e121347fd10d711a5d262f8105
auk003150 <69250701+auk003150@users.noreply.github.com>
2022-03-28 11:13:22 +0800

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1372564.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 74696458, 74701103 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/74696433/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/74696434/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/74696432/1/ammo.js', 'preload' : true},
+];
